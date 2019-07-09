99701454ab94416000f17fee177d7c5568a457dc
YafeiQQ <yafeigeng1228@gmail.com>
2019-07-08 22:42:26 -0700

    add Restful API get method using axios to fetch data

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSearchSubmit(term){
-        console.log(term);
-    }
+    state = {
+        images: []
+    };
+
+    onSearchSubmit = async (term) => {
+        const responds = await unsplash.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization:
+                    'Client-ID 7e0e830ffa64c0939f150d5f3bac06907f65017f48e4d7d146b801ab294dfa3c'
+            }
+        });
+
+        console.log(this);
+        this.setState({images: responds.data.results});
+    };
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images
             </div>
         )
     }
---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default  axios.create({
+    headers: {
+        Authorization:
+            'Client-ID 7e0e830ffa64c0939f150d5f3bac06907f65017f48e4d7d146b801ab294dfa3c'
+    }
+});
